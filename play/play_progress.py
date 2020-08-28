4d6cbd052be99e0276b5434e25550962492c5c86
Kris Napiontek <krzysztof.napiontek@huawei.com>
2020-08-28 22:33:56 +0100

    play with progress

--- play/play_progress.py
+++ play/play_progress.py
@@ -1,7 +1,12 @@
 import sys
 import time
 
-for i in range(100):
-    time.sleep(1)
-    sys.stdout.write(f'\rcomplete: {i}%%')
+
+def progress(i: int, max: int):
+    sys.stdout.write(f'\rcomplete: {100 * i / max:.1f}%')
     sys.stdout.flush()
+
+
+for i in range(134):
+    time.sleep(0.3)
+    progress(i, 133)
