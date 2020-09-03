0e849bc47e876fcf11aa4209a895f0e72e65e482
Kris Napiontek <krzysztof.napiontek@huawei.com>
2020-09-03 12:55:13 +0100

    yahoo play 1

--- play/play_yahoo.py
+++ play/play_yahoo.py
@@ -23,16 +23,31 @@ INTERVAL = '1d'
 LOGIN_URL = f'https://finance.yahoo.com/quote/{SYMBOL}/?p={SYMBOL}'
 PATTERN = re.compile('"CrumbStore":{"crumb":"(.+?)"}')
 
+KEYS = {
+    'Date': 'date',
+    'Open': 'open',
+    'Close': 'close',
+    'Low': 'log',
+    'High': 'high',
+    'Volume': 'volume'
+}
+
 with requests.Session() as session:
     response = session.get(LOGIN_URL)
     assert response.status_code == 200, response.text
     found = re.search(PATTERN, response.text)
     if found:
         crumb = found.group(1)
-        url = f'https://query1.finance.yahoo.com/v7/finance/download/{SYMBOL}?period1={PERIOD1}&period2={PERIOD2}&interval={INTERVAL}&events=history&crumb={crumb}'
-        print(url)
-        response = session.get(url)
-        data = [row for row in csv.DictReader(StringIO(response.text))]
+        url = f'https://query1.finance.yahoo.com/v7/finance/download/{SYMBOL}'
+        params = {
+            'period1': PERIOD1,
+            'period2': PERIOD2,
+            'interval': INTERVAL,
+            'events': 'history',
+            'crumb': crumb
+        }
+        response = session.get(url, params=params)
+        data = [{v: item[k] for k, v in KEYS.items()} for item in csv.DictReader(StringIO(response.text))]
         print(json.dumps(data, indent=2))
     else:
         print('Problem')
