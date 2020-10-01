008a748220db46f097a1f1bed238dd4e6b5bbe29
Kris Napiontek <krzysztof.napiontek@huawei.com>
2020-10-01 15:13:38 +0100

    final minor fixes

--- tests/test_data.py
+++ tests/test_data.py
@@ -8,7 +8,7 @@ def test_exchanges():
     with store.Exchanges() as db_exchanges:
         for name in config.ACTIVE_EXCHANGES:
             instruments = db_exchanges[name]
-            assert len(instruments) > 64
+            assert len(instruments) > 16
             for instrument in instruments:
                 document = {k: v for k, v in instrument.items() if k not in ('_rev', '_id', '_key')}
                 jsonschema.validate(document, store.EXCHANGE_SCHEMA['rule'])
