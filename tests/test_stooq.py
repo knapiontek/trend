a5fd8d637adadade884fea04315e317f2da8e90d
Kris Napiontek <krzysztof.napiontek@huawei.com>
2020-12-04 11:00:27 +0000

    DE stooq lost its data

--- tests/test_stooq.py
+++ tests/test_stooq.py
@@ -7,10 +7,12 @@ def test_series():
     interval = tool.INTERVAL_1D
     dt_from = datetime(2020, 2, 2, tzinfo=timezone.utc)
     dt_to = datetime(2020, 2, 4, tzinfo=timezone.utc)
-    with stooq.Session({'XETRA': [interval]}) as session:
-        series = session.series('SIE.XETRA', dt_from, dt_to, interval)
+    with stooq.Session({'WSE': [interval]}) as session:
+        series = session.series('KGH.WSE', dt_from, dt_to, interval)
 
     closing_prices = [(tool.ts_format(s.timestamp), s.close, s.volume) for s in series]
 
-    assert closing_prices == [('2020-02-03 00:00:00 +0000', 107.41, 2466371),
-                              ('2020-02-04 00:00:00 +0000', 109.11, 2416826)]
+    assert closing_prices == [
+        ('2020-02-03 00:00:00 +0000', 92.6, 484464),
+        ('2020-02-04 00:00:00 +0000', 96.44, 708829)
+    ]
