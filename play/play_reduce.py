cf8c71831da41e28813fce284aa91d3a1855542f
Kris Napiontek <krzysztof.napiontek@huawei.com>
2020-12-04 16:29:42 +0000

    clean play_reduce()

--- play/play_reduce.py
+++ play/play_reduce.py
@@ -1,20 +1,17 @@
 from collections import deque
-from typing import List, Sized, Tuple, Optional, Iterable
+from typing import List, Iterable
 
 import matplotlib.pyplot as plt
-from more_itertools import windowed
 
 from src import tool, yahoo
 
 
-def i_windowed(sized: Sized, size: int):
-    return windowed(range(len(sized)), size)
-
-
-def read_data(timestamp) -> List[tool.Clazz]:
+def read_data(begin: int, end: int) -> List[tool.Clazz]:
     interval = tool.INTERVAL_1D
     with yahoo.SecuritySeries(interval) as security_series:
-        abc_series = [s for s in security_series['ABC.NYSE'] if s.timestamp < timestamp]
+        abc_series = security_series['ABC.NYSE']
+        print(abc_series[-1].timestamp)
+        abc_series = [s for s in abc_series if begin <= s.timestamp <= end]
     for s in abc_series:
         s.grade = 0.0
         s.timestamp /= 1e6
@@ -28,69 +25,13 @@ def plot_series(series: Iterable[tool.Clazz], grade: float, label: str, color: s
     plt.plot(timestamps, closes, style, label=label, color=color, linewidth=1, markersize=2)
 
 
-def reduce1(series: List[tool.Clazz], max_grade: int) -> Tuple[List[tool.Clazz], List[tool.Clazz]]:
-    min_spikes: List[tool.Clazz] = []
-    max_spikes: List[tool.Clazz] = []
-    w_size = 3
-    if len(series) >= w_size:
-        closes = [s.close for s in series]
-        min_val = min(closes)
-        max_val = max(closes)
-        grade = round((max_val - min_val) / max_grade)
-        for i1, i2, i3 in i_windowed(series, w_size):
-            c1, c2, c3 = [series[i].close for i in [i1, i2, i3]]
-            if c2 == min(c1, c2, c3):
-                min_spikes = [s for s in min_spikes if s.close < c2]
-                min_spikes.append(series[i2])
-                for s in max_spikes:
-                    s.grade = max(s.grade, abs(s.close - c2))
-            if c2 == max(c1, c2, c3):
-                max_spikes = [s for s in max_spikes if s.close > c2]
-                max_spikes.append(series[i2])
-                for s in min_spikes:
-                    s.grade = max(s.grade, abs(s.close - c2))
-        return min_spikes, max_spikes
-    return [], []
+def reduce(series: List[tool.Clazz], limit: float) -> Iterable[tool.Clazz]:
+    if series and limit > 0:
+        limit *= series[-1].close / 100
+    else:
+        return series
 
-
-def show1(timestamp):
-    abc_series = read_data(timestamp)
-    plot_series(abc_series, 0.0, 'ABC', 'grey')
-
-    min_spikes, max_spikes = reduce1(abc_series, 10)
-    plot_series(min_spikes, 0.0, 'min', 'green', 'o')
-    plot_series(max_spikes, 0.0, 'max', 'red', 'o')
-    plot_series(min_spikes, 20.0, 'min20', 'orange', 'o')
-    plot_series(max_spikes, 20.0, 'max20', 'blue', 'o')
-
-    plt.legend(loc='upper left')
-    plt.grid()
-    plt.tight_layout()
-    plt.show()
-
-
-def test1():
-    for ts in [1524750000, 1546000000, 1700000000]:
-        show1(ts)
-
-
-def clean(series: List[tool.Clazz]) -> List[tool.Clazz]:
-    reduced: List[Optional[tool.Clazz]] = series[:]
-    for i1, i2, i3 in i_windowed(series, 3):
-        close1, close2, close3 = [series[i].close for i in [i1, i2, i3]]
-        if close1 <= close2 <= close3:
-            reduced[i2] = None
-            if close1 == close3:
-                reduced[i1] = None
-        elif close1 >= close2 >= close3:
-            reduced[i2] = None
-            if close1 == close3:
-                reduced[i1] = None
-    return list(filter(None, reduced))
-
-
-def reduce2(series: List[tool.Clazz], limit: int) -> Iterable[tool.Clazz]:
-    queue = deque()
+    queue = deque()  # reversed to series
     for s in reversed(series):
         if len(queue) >= 2:
             close1, close2, close3 = queue[1].close, queue[0].close, s.close
@@ -113,11 +54,11 @@ def reduce2(series: List[tool.Clazz], limit: int) -> Iterable[tool.Clazz]:
     return queue
 
 
-def show2(timestamp, grade):
-    abc_series = read_data(timestamp)
+def show(begin: int, end: int, limit: float):
+    abc_series = read_data(begin, end)
     plot_series(abc_series, 0.0, 'ABC', 'grey')
 
-    reduced = reduce2(abc_series, grade)
+    reduced = reduce(abc_series, limit)
     plot_series(reduced, 0.0, 'reduced', 'blue', 'o')
 
     plt.legend(loc='upper left')
@@ -126,13 +67,9 @@ def show2(timestamp, grade):
     plt.show()
 
 
-def test2():
-    show2(1524750000, 0.1)
-    show2(1524750000, 0.5)
-    show2(1524750000, 1)
-    show2(1524750000, 11)
-    show2(1524750000, 21)
+def execute():
+    show(1514851200, 1606953600, 4)
 
 
 if __name__ == '__main__':
-    test2()
+    execute()
