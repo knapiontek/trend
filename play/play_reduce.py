f1d0130ffeac3de381dbaf68a1c17d6333a34888
Kris Napiontek <knapiontek@gmail.com>
2021-02-11 13:48:10 +0000

    play with subplots

--- play/play_reduce.py
+++ play/play_reduce.py
@@ -3,7 +3,7 @@ from enum import Enum, IntEnum, auto
 from typing import List
 
 import matplotlib.pyplot as plt
-from matplotlib import ticker
+from matplotlib import ticker, axes
 
 from src import tool, swings, analyse, stooq
 from src.clazz import Clazz
@@ -21,6 +21,7 @@ def read_series(symbol: str, interval: timedelta, begin: int, end: int) -> List[
 
 class Color(Enum):
     grey = 'grey'
+    silver = 'silver'
     yellow = 'yellow'
     orange = 'orange'
     red = 'red'
@@ -44,62 +45,15 @@ class Color(Enum):
         return colors[score - 1]
 
 
-def plot_line(xs: List[float], ys: List[float], label: str, color: Color):
-    plt.plot(xs, ys, '-', label=label, color=color.value, linewidth=1)
+fig, (ax1, ax2) = plt.subplots(2, 1, sharex=True)
 
 
-def plot_dots(xs: List[float], ys: List[float], label: str, color: Color, size: int):
-    plt.plot(xs, ys, 'o', label=label, color=color.value, linewidth=1, markersize=size)
+def plot_line(ax: axes.Axes, xs: List[float], ys: List[float], label: str, color: Color):
+    ax.plot(xs, ys, '-', label=label, color=color.value, linewidth=1)
 
 
-def plot_bars(series: List[Clazz]):
-    results = []
-    for s in series:
-        value1, value2 = (s.low, s.high) if s.close > s.open else (s.high, s.low)
-        results += [Clazz(timestamp=s.timestamp, value=s.open),
-                    Clazz(timestamp=s.timestamp, value=value1),
-                    Clazz(timestamp=s.timestamp, value=value2),
-                    Clazz(timestamp=s.timestamp, value=s.close)]
-    plot_line([s.timestamp for s in results], [s.value for s in results], label='bars', color=Color.grey)
-
-
-def plot_vma(series: List[Clazz], w_size: int, color: Color):
-    vma_name = f'vma-{w_size}'
-    plot_line([s.timestamp for s in series], [s.get(vma_name) for s in series], label=vma_name, color=color)
-
-
-def plot_valid_swings(series: List[Clazz], score: int):
-    results = []
-    for s in series:
-        if score <= s.valid_low_score:
-            results += [Clazz(timestamp=s.timestamp, value=s.low)]
-        if score <= s.valid_high_score:
-            results += [Clazz(timestamp=s.timestamp, value=s.high)]
-    color = Color.from_score(score)
-    plot_dots([s.timestamp for s in results], [s.value for s in results],
-              label=f'score-{score} [{int(100 * swings.limit_ratio(score)):03}%]',
-              color=color, size=1 + score)
-
-
-def plot_candidate_swings(series: List[Clazz], score: int):
-    results = []
-    for s in series:
-        if score <= s.low_score:
-            results += [Clazz(timestamp=s.timestamp, value=s.low)]
-        if score <= s.high_score:
-            results += [Clazz(timestamp=s.timestamp, value=s.high)]
-    color = Color.from_score(score)
-    plot_dots([s.timestamp for s in results], [s.value for s in results],
-              label=f'score-{score} [{int(100 * swings.limit_ratio(score)):03}%]',
-              color=color, size=1 + score)
-
-
-def plot_strategy(series: List[Clazz]):
-    ts = [s.timestamp for s in series]
-    plot_dots(ts, [s.low if s.low_score else None for s in series], label='Candidate', color=Color.orange, size=2)
-    plot_dots(ts, [s.test.drop for s in series], label='Drop', color=Color.red, size=4)
-    plot_dots(ts, [s.test.long for s in series], label='Long', color=Color.green, size=4)
-    plot_dots(ts, [s.test.short for s in series], label='Short', color=Color.blue, size=4)
+def plot_dots(ax: axes.Axes, xs: List[float], ys: List[float], label: str, color: Color, size: int):
+    ax.plot(xs, ys, 'o', label=label, color=color.value, linewidth=1, markersize=size)
 
 
 def show_widget(symbol: str, begin: int, end: int):
@@ -109,47 +63,99 @@ def show_widget(symbol: str, begin: int, end: int):
         else:
             return DateTime.from_timestamp(timestamp).strftime('%Y-%m-%d')
 
-    plt.title(f'{symbol} [{format_date(begin)} - {format_date(end)}]')
-    plt.legend(loc='upper left')
-    plt.grid()
-    plt.tight_layout()
-    plt.gca().set_facecolor('silver')
-    plt.gca().xaxis.set_major_formatter(ticker.FuncFormatter(format_date))
+    for a in (ax1, ax2):
+        a.set(title=f'{symbol} [{format_date(begin)} - {format_date(end)}]')
+        a.legend(loc='upper left')
+        a.grid()
+        a.set_facecolor(Color.silver.value)
+        a.xaxis.set_major_formatter(ticker.FuncFormatter(format_date))
+    fig.tight_layout()
     plt.show()
 
 
 # show
 
+
+def plot_bars(ax: axes.Axes, series: List[Clazz]):
+    results = []
+    for s in series:
+        value1, value2 = (s.low, s.high) if s.close > s.open else (s.high, s.low)
+        results += [Clazz(timestamp=s.timestamp, value=s.open),
+                    Clazz(timestamp=s.timestamp, value=value1),
+                    Clazz(timestamp=s.timestamp, value=value2),
+                    Clazz(timestamp=s.timestamp, value=s.close)]
+    plot_line(ax, [s.timestamp for s in results], [s.value for s in results], label='bars', color=Color.grey)
+
+
+def plot_vma(ax: axes.Axes, series: List[Clazz], w_size: int, color: Color):
+    vma_name = f'vma-{w_size}'
+    plot_line(ax, [s.timestamp for s in series], [s.get(vma_name) for s in series], label=vma_name, color=color)
+
+
+def plot_valid_swings(ax: axes.Axes, series: List[Clazz], score: int):
+    results = []
+    for s in series:
+        if score <= s.valid_low_score:
+            results += [Clazz(timestamp=s.timestamp, value=s.low)]
+        if score <= s.valid_high_score:
+            results += [Clazz(timestamp=s.timestamp, value=s.high)]
+    color = Color.from_score(score)
+    plot_dots(ax, [s.timestamp for s in results], [s.value for s in results],
+              label=f'score-{score} [{int(100 * swings.limit_ratio(score)):03}%]',
+              color=color, size=1 + score)
+
+
+def plot_candidate_swings(ax: axes.Axes, series: List[Clazz], score: int):
+    results = []
+    for s in series:
+        if score <= s.low_score:
+            results += [Clazz(timestamp=s.timestamp, value=s.low)]
+        if score <= s.high_score:
+            results += [Clazz(timestamp=s.timestamp, value=s.high)]
+    color = Color.from_score(score)
+    plot_dots(ax, [s.timestamp for s in results], [s.value for s in results],
+              label=f'score-{score} [{int(100 * swings.limit_ratio(score)):03}%]',
+              color=color, size=1 + score)
+
+
+def plot_strategy(ax: axes.Axes, series: List[Clazz]):
+    ts = [s.timestamp for s in series]
+    plot_dots(ax, ts, [s.low if s.low_score else None for s in series], label='Candidate', color=Color.orange, size=2)
+    plot_dots(ax, ts, [s.test.drop for s in series], label='Drop', color=Color.red, size=4)
+    plot_dots(ax, ts, [s.test.long for s in series], label='Long', color=Color.green, size=4)
+    plot_dots(ax, ts, [s.test.short for s in series], label='Short', color=Color.blue, size=4)
+
+
 def show_vma(symbol: str, interval: timedelta, begin: int, end: int):
     series = read_series(symbol, interval, begin, end)
-    plot_bars(series)
+    plot_bars(ax1, series)
 
     for w_size, color in ((30, Color.green), (90, Color.blue)):
         analyse.vma(series, w_size)
-        plot_vma(series, w_size, color)
+        plot_vma(ax1, series, w_size, color)
 
     show_widget(symbol, begin, end)
 
 
 def show_valid_swings(symbol: str, interval: timedelta, begin: int, end: int):
     series = read_series(symbol, interval, begin, end)
-    plot_bars(series)
+    plot_bars(ax1, series)
 
     swings.calculate(series)
     for score in range(1, 9):
-        plot_valid_swings(series, score)
+        plot_valid_swings(ax1, series, score)
 
     show_widget(symbol, begin, end)
 
 
 def show_candidate_swings(symbol: str, interval: timedelta, begin: int, end: int):
     series = read_series(symbol, interval, begin, end)
-    plot_bars(series)
+    plot_bars(ax1, series)
 
     reduced = swings.init(series)
     for score in (1, 3):
         reduced = swings.reduce(reduced, score)
-        plot_candidate_swings(series, score)
+        plot_candidate_swings(ax1, series, score)
 
     show_widget(symbol, begin, end)
 
@@ -166,7 +172,7 @@ class State(IntEnum):
 
 def show_strategy(symbol: str, interval: timedelta, begin: int, end: int):
     series = read_series(symbol, interval, begin, end)
-    plot_bars(series)
+    plot_bars(ax1, series)
 
     reduced = swings.init(series)
     swings.reduce(reduced, Swing.DROP.value)
@@ -196,7 +202,7 @@ def show_strategy(symbol: str, interval: timedelta, begin: int, end: int):
                 lowest_close = None
                 continue
 
-    plot_strategy(series)
+    plot_strategy(ax1, series)
 
     show_widget(symbol, begin, end)
 
